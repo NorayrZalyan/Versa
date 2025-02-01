a32fdaa282b52c70d113af2e5447db9a75f44a3a
Norayr Zalyan <norayr.zalyan2009@gmail.com>
2025-02-01 10:10:09 +0400

    room layout beta

--- app/src/main/java/com/example/versa/Room.java
+++ app/src/main/java/com/example/versa/Room.java
@@ -3,14 +3,15 @@ package com.example.versa;
 public class Room {
 
     private String name;
-    private static int id = 0;
+    private static int nextid = 0;
+    private int id;
 
     public Room(){
-        id++;
+
     }
     public Room(String name){
         this.name = name;
-        Room.id = ++Room.id;
+        this.id = ++nextid;
     }
 
     public String getName() {
@@ -25,7 +26,7 @@ public class Room {
         this.name = name;
     }
 
-    public static void setId(int id) {
-        Room.id = id;
+    public void setId(int id) {
+        this.id = id;
     }
 }
